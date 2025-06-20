4e7428a07bfcc10d997082d48a714a3558b685a6
RAVIN3 <84334750+RAVIN3@users.noreply.github.com>
2025-06-20 21:12:33 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -1,16 +1,18 @@
 def gitDownload(repo)
 {                   
-  git "https://github.com/RAVIN3/repo.git"    
+  git "https://github.com/RAVIN3/${repo}.git"    
 }
 
 def buildArtifect()
 {
   sh "mvn package"
 }
+
 def deployTomcat(jobname, ipaddress, contextpath)
 {
   sh "scp  /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${iaddress}:/var/lib/tomcat10/webapps/${context}.war"
 }
+
 def testing(repo,jobname)
 {
   git "https://github.com/IntelliqDevops/${repo}.git"
